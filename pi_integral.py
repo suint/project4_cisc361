7aade870cbfc96ac665d6a55eb08b1225e5e7073
Betty Zhang <bettyzhang@Bettys-Air.fios-router.home>
2019-12-03 17:28:41 -0500

    integral method

---
+++ pi_integral.py
@@ -0,0 +1,13 @@
+//Input N (no. of intervals, a = 0  and b=1 are limits)
+int h = (b-a)/N;
+//start the clock
+//double time
+int sum = f(a)+f(b)+4*f(a+h)
+insert #pragma
+for i in range(3,N-1):
+  x = 2*f(a+(i-1)*h + 4*f(a+i*h)
+  sum = sum +x
+  i = i+2
+double result = (h/3)*sum
+//stop the clock
+print 'time is ' + time + ' and the result is' + result
