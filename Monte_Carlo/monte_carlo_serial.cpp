bf26b02f8d1487c944c2dfdcaa24373df96fa6ab
Yufan Wang <leeff@ubuntu-1804.udel.edu>
2019-12-03 21:38:28 -0500

    finished M.C. Serial and OMP vers

---
+++ Monte_Carlo/monte_carlo_serial.cpp
@@ -0,0 +1,54 @@
+// from https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
+
+/* C++ program for estimation of Pi using Monte 
+   Carlo Simulation */
+#include <bits/stdc++.h>
+#include <math.h>
+#include <omp.h>
+#include <cmath>
+
+// Defines precision for x and y values. More the 
+// interval, more the number of significant digits 
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    const int INTERVAL = atoi(argv[1]);
+
+    int i;
+    double rand_x, rand_y, pi;
+    int circle_points = 0;
+    unsigned int tmp;
+
+    // Initializing rand()
+    srand(time(NULL));
+
+    // Total Random numbers generated = possible x
+    // values * possible y values
+
+    double st = omp_get_wtime();
+
+    for (i = 0; i < INTERVAL; i++) {
+
+        // Randomly generated x and y values
+        rand_x = double(rand_r(&tmp)) / RAND_MAX;
+        rand_y = double(rand_r(&tmp)) / RAND_MAX;
+
+        // Checking if (x, y) lies inside the define
+        // circle with R=1
+        if (rand_x * rand_x + rand_y * rand_y <= 1)
+            circle_points++;
+    }
+    
+    // Final Estimated Value
+    pi = 4*(double(circle_points) / INTERVAL);
+
+    double runtime = omp_get_wtime() - st;
+    cout << " Number of threads: " << atoi(argv[1]) << endl;
+    cout << " Final Estimation of Pi = " << pi << endl;
+    cout << " Error = " << abs(M_PI - pi) << endl;
+    printf(" total: %f s\n", runtime);
+
+    return 0;
+}
