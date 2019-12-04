d865698a33b7fa0d77e05428c4bb050caa537295
kelly mi <lui.ysia@gmail.com>
2019-12-04 04:01:22 +0000

    pi integral cpp

--- pi_integral.cpp
+++ pi_integral.cpp
@@ -1,8 +1,19 @@
 #include <omp.h>
 
-double pi(int N){
-    double sum = 0.0;
-    int a = 0;
-    int b = 1;
-    int h = (b-a)/N;
+// Forward declaration of sqrt
+double sqrt(double x);
+
+double pi(int N) {
+
+	double sum = 0.0;
+	#pragma omp parallel default(none) shared(N, sum)
+	{
+    omp_set_num_threads(8);
+	#pragma omp for reduction(+: sum) schedule(auto)
+	for (int i = 0; i < N; i++) {
+		double x = (i + 0.5) / N;
+		sum += sqrt(1 - x * x);
+	}
+	} // end parallel
+	return 4.0 / N * sum;
 }
